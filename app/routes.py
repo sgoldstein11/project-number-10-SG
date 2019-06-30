e21d448d03aad54007b515c281a3e457df764385
EC2 Default User <ec2-user@ip-172-31-37-186.us-east-2.compute.internal>
2019-06-30 18:38:40 +0000

    test for deployment

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template, request
+from app.models import model, formopener
+
+
+@app.route('/')
+@app.route('/younger_index')
+def index():
+    return render_template('younger_index.html')
+
+
+@app.route('/younger_results', methods = ['GET', 'POST'])
+def results ():
+    if request.method == 'GET':
+        return "Please use the form <br> <a href='/younger_index'>Click here to go home!</a>"
+        
+    else:
+        userdata = formopener.dict_from(request.form)
+        nickname = userdata ['nickname']
+        print(userdata)
+        final_results, img_url = model.final_results(userdata) 
+        return render_template('younger_results.html',nickname=nickname, final_results=final_results, img_src=img_url)
